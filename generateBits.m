d795472645bbc5c1f9e1a06f7612f63e2f894418
ahmedosama07 <ahmed.osama8282@gmail.com>
2022-12-28 03:54:51 +0200

    Using some generateBits() function
    Hope this gonna work

--- generateBits.m
+++ generateBits.m
@@ -1,7 +1,7 @@
 function signal = generateBits(N, amp)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-signal = zeros(1, N);
-signal = amp * randi([0 1], 1, N);
+signal = zeros(N, 1);
+signal = amp * randi([0 1], N, 1);
 end
 
--- main.m
+++ main.m
@@ -1,38 +1,26 @@
-% bits = generateBits(6, 1);
-% n = bits .* randn(size(bits));
-% b = bits + n;
-% % b = noise(bits);
-% l1=computeBER(bits, b);
-% for i = 1 : 1 : 6
-%     if b(1, i) > 0.5
-%         b(1, i) = 1;
-%     else
-%         b(1, i) = 0;
-%     end
-% end
-%l3=computeBER(bits, b);
-
-
+n_bits = 1e5;
+amplitude = 5;
 rng('default'); % For reproducibility
-X = directbinornd(1,0.3,1e4,1);
-Xn = awgn(X, 0.3);
+% X = directbinornd(1,0.3,n_bits,1);
+% Xn = awgn(X, 0.3);
+X = generateBits(n_bits, amplitude);
+Xn = wgn(n_bits, 1, 0.9, 'linear');
+% histogram(X,101);
 
-histogram(X,101);
-l1=computeBER(X, Xn);
+err = 0;
 
-for i = 1 : 1 : 1e4
+for i = 1 : 1 : n_bits
     if Xn(i, 1) > 0.5
         Xn(i, 1) = 1;
     else
         Xn(i, 1) = 0;
     end
 end
-err = 0;
-for i = 1 : 1 : 1e4
+
+for i = 1 : 1 : n_bits
     if Xn(i, 1) == X(i, 1)
         err = err + 1;
     end
 end
-err/(1e4)
-l2=computeBER(X, Xn);
 
+BER = err / n_bits;
