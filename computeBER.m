1610a9f022d5a19758cabd7f986c6913b31fe8f2
ahmedosama07 <ahmed.osama8282@gmail.com>
2022-12-29 23:40:31 +0200

    Final version

--- computeBER.m
+++
@@ -1,9 +0,0 @@
-function BER = computeBER(signal ,noisySignal)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-BERVector=noisySignal - signal;
-BERVectorABS=abs(BERVector);
-NumWrongBits=sum(BERVectorABS);
-BER=NumWrongBits/length(signal);
-end
-
--- directbinornd.m
+++
@@ -1,8 +0,0 @@
-function X = directbinornd(N,p,m,n)
-    X = zeros(m,n); % Preallocate memory
-    disp(m*n);
-    for i = 1:m*n
-        u = rand(N,1);
-        X(i) = sum(u < p);
-    end
-end
